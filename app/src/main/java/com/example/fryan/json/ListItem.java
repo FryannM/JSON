aee98d6ab5717e7e8adc2776ab49089b1f09a42c
Fryann <fryann14@hotmail.com>
2018-06-28 21:47:32 -0400

    Getting ready JSON API

--- app/src/main/java/com/example/fryan/json/ListItem.java
+++ app/src/main/java/com/example/fryan/json/ListItem.java
@@ -4,10 +4,26 @@ public class ListItem {
 
     private String head;
     private String desc;
+    private String imageUrl;
+    private  String Realname;
+    private String Team;
+    private  String Firstappearance;
+    private  String Createdby;
+    private String Publisher;
 
-    public ListItem(String head, String desc) {
+
+
+
+    public ListItem(String head, String desc, String imageUrl,String realname, String team, String firstappearance, String createdby, String Publisher) {
         this.head = head;
         this.desc = desc;
+        this.imageUrl = imageUrl;
+        Realname = realname;
+        Team = team;
+        Firstappearance = firstappearance;
+        Createdby = createdby;
+        this.Publisher = Publisher;
+
     }
 
     public String getHead() {
@@ -18,5 +34,27 @@ public class ListItem {
         return desc;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
+    public String getRealname() {
+        return Realname;
+    }
+
+    public String getTeam() {
+        return Team;
+    }
+
+    public String getFirstappearance() {
+        return Firstappearance;
+    }
+
+    public String getCreatedby() {
+        return Createdby;
+    }
+
+    public String getPublisher() {
+        return Publisher;
+    }
 }
--- app/src/main/java/com/example/fryan/json/MainActivity.java
+++ app/src/main/java/com/example/fryan/json/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.fryan.json;
 
+import android.app.LauncherActivity;
+import android.app.ProgressDialog;
+import android.net.sip.SipSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  static final String  URL_DATA ="https://simplifiedcoding.net/demos/marvel/";
 
     private RecyclerView recyclerView;
     private  RecyclerView.Adapter adapter;
@@ -25,23 +43,58 @@ public class MainActivity extends AppCompatActivity {
 
          recyclerView = (RecyclerView) findViewById(R.id.RecycleID);
          recyclerView.setHasFixedSize(true);
-
          recyclerView.setLayoutManager( new LinearLayoutManager(this));
-
          listItemList = new ArrayList<>();
+          loadRecycleViewData();
 
 
-          for (int i=0; i< 10 ; i++)
-          {
-              ListItem listItem= new ListItem(
-              "Heading" + i+ 1,
-              "Dummy Data"
-              );
-              listItemList.add(listItem);
-          }
+    }
 
-           adapter = new MyAdapter(listItemList,this);
-          recyclerView.setAdapter(adapter);
+    private void  loadRecycleViewData(){
 
+        ProgressDialog progressDialog = new ProgressDialog(this);
+
+        progressDialog.setMessage("Loading data......");
+        progressDialog.show();
+
+        StringRequest  stringRequest = new StringRequest(Request.Method.GET,
+                URL_DATA, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+
+                    JSONArray array= jsonObject.getJSONArray("Heroes");
+
+                     for (int i =0; i< array.length(); i++)
+                     {
+                          JSONObject o =  array.getJSONObject(i);
+                          ListItem listItem = new ListItem(
+                                  o.getString("Name"),
+                                  o.getString("RealName"),
+                                  o.getString("Team"),
+                                  o.getString("Firstappearance"),
+                                  o.getString("Createdby"),
+                                  o.getString("Publisher"),
+                                  o.getString("imageurl"),
+                                  o.getString("Bio")
+                          );
+                     }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+         requestQueue.add(stringRequest);
     }
 }
